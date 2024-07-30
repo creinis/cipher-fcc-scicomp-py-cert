7ac72bc06a3fefb1e1d6ba4bace75308e89c933b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 11:57:50 +0200

    feat: declare another variable called text and assign the string Hello World to this variable

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # Create a variable called number and assign it the value 5
 
 number = 5
+
+# Step 2
+# Delete your number variable and its value. 
+# Then, declare another variable called text and assign the string Hello World to this variable.
+
+text = 'Hello World'
