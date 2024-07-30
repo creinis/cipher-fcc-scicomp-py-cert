6e2798d66cd724fff367ae5c0861b2ffffd90f0e
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 15:29:31 +0200

    feat: vig: define function passing message and key as arguments, declaring key_index, alphabel string and final_message; returning final_message as the output of the function

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,4 +1,11 @@
 text = ''
 custom_key = ''
 
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+    
+    return final_message
+
 
