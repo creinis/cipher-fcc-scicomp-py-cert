a756c9e91f5689277d7012bb24ff6d32ac5997cf
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:22:31 +0200

    feat: print() call to print text.lower()

--- main.py
+++ main.py
@@ -84,3 +84,10 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0])
 print(index)
 
+# Step 14
+
+# find() returns the index of the matching character inside the string. If the character is not found, it returns -1. As you can see, the first character in text, uppercase "H", is not found, since alphabet contains only lowercase letters.
+# You can transform a string into its lowercase equivalent with the lower() function. Add another print() call to print text.lower() and see the output.
+
+print(text.lower())
+
