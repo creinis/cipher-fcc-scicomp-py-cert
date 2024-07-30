43cf2b85cb5540674c317fa238c3b0df5a4c5558
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:53:10 +0200

    feat: new function called encrypt that takes message and key parameters, and use pass to fill the function body.

--- main.py
+++ main.py
@@ -697,4 +697,17 @@ if char.isalpha():
 if not char.isalpha():
     final_message += char
 
+# Step 78
+
+# The pass keyword can be used as a placeholder for future code. 
+# It does not have any effect in your code but it can save you from errors you would get in case of incomplete code:
+
+def foo():
+    pass
+
+# Calling vigenere with 1 to encrypt and -1 to decrypt is fine but it might be a little bit cryptic. 
+# Create a new function called encrypt that takes message and key parameters, and use pass to fill the function body.
+
+def encrypt(message, key):
+    pass
 
