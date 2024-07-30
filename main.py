414d902cebbdc70407598e0ccb2ca287a7a5f9a0
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:42:44 +0200

    feat: declare a function called caesar and indent the following lines, so they become the function body.

--- main.py
+++ main.py
@@ -432,4 +432,35 @@ for char in text.lower():
 print('plain text:', text)
 print('encrypted text:', encrypted_text)
 
+# Step 46 - Function def
+
+# A function is essentially a reusable block of code. 
+# You have already met some built-in functions, like print(), find() and len(). 
+# But you can also define custom functions like this:
+
+    # def function_name():
+    #     <code>
+
+# A function declaration starts with the def keyword followed by the function name — a valid variable name — and a pair of parentheses. 
+# The declaration ends with a colon.
+# Right after your shift variable, declare a function called caesar and indent the following lines, so they become the function body.
+
+text = 'Hello Zaira'
+shift = 3
+def caesar():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in text.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + shift) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', text)
+    print('encrypted text:', encrypted_text)
+
+caesar()
+
 
