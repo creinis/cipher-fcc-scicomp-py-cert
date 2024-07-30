0549c21f3ebe2520c6a2b7c5e1266f57f4397320
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:24:14 +0200

    feat: write a for loop to iterate over text

--- main.py
+++ main.py
@@ -114,3 +114,20 @@ print(index)
 shifted = alphabet[index + shift]
 print(shifted)
 
+# Step 18 - Loop
+
+# Repeating this process for each character in text can be tedious. Thankfully, you can simplify it using a loop. 
+# A loop allows you to systematically go through a sequence of elements and execute actions on each one.
+# In this case, you'll employ a for loop. Here's how you can iterate over text:
+
+        #  for i in text:
+
+# for is the keyword denoting the loop type. 
+# i is a variable that sequentially takes the value of the elements in text. The statement ends with a colon, :.
+# Remove everything after the alphabet line. Then write a for loop to iterate over text.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# for i in text:
+
