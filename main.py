7c18164c99206dadf031a73f1f77999ecc0c4e74
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:38:55 +0200

    feat: add an else clause after encrypted_text += char and indent all the subsequent lines of code.

--- main.py
+++ main.py
@@ -342,4 +342,33 @@ for char in text.lower():
     encrypted_text += alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 40
+
+# A conditional statement can also have an else clause. 
+# This clause can be added to the end of an if statement to execute alternative code if the condition is false:
+
+    # if condition:
+    #     <code>
+    # else:
+    #     <code>
+
+# As you can see in your output, when the loop iterations reach the space, a space is added to the encrypted string. 
+# Then the code outside the if block executes and a c is added to the encrypted string.
+# To fix it, add an else clause after encrypted_text += char and indent all the subsequent lines of code.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = index + shift
+        encrypted_text += alphabet[new_index]
+        print('char:', char, 'encrypted text:', encrypted_text)
+
+
 
