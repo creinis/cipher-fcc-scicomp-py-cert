7d899e1d9827cb7431c330e1c26dd7cc5911a652
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:38:18 +0200

    feat: addition assignment operator to add the space to the current value of encrypted_text.

--- main.py
+++ main.py
@@ -325,4 +325,21 @@ for char in text.lower():
     encrypted_text += alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 39
+
+# Now, instead of printing space!, use the addition assignment operator to add the space to the current value of encrypted_text.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+
 
