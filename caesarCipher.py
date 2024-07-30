0cb4156ad351e8bca33314cdcae963a400c92c61
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 13:58:51 +0200

    feat: caesar: for loop every char in message to lower, start if else block

--- caesarCipher.py
+++ caesarCipher.py
@@ -5,6 +5,11 @@ def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
     
+    for char in message.lower():
+        if:
+            
+        else:
+    
     print('plain text:', message)
     print('encrypted text:', encrypted_text)
     
