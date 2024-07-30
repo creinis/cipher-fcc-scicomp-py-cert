2dc820e048fda3100dd2e99eb81128c04811821f
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:37:48 +0200

    feat: replace print(char == ' ') with an if statement.  If statement should evaluate to True if char is an empty space and False otherwise. If body, print the string space

--- main.py
+++ main.py
@@ -299,5 +299,30 @@ for char in text.lower():
     encrypted_text += alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 38 - if
+
+# Currently, spaces get encrypted as c. 
+# To maintain the original spacing in the plain message, you'll require a conditional if statement. 
+# This is composed of the if keyword, a condition, and a colon :.
+
+# if <condition>:
+#     <code>
+
+# At the top of your for loop, replace print(char == ' ') with an if statement. 
+# The condition of this if statement should evaluate to True if char is an empty space and False otherwise. 
+# Inside the if body, print the string space!. Remember to indent this line.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        print('space!')
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
 
 
