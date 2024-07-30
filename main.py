4e694e0f586948ae6f75f804674380237a12cc73
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:24:50 +0200

    feat: Give your for loop a body by adding a call to print(i)

--- main.py
+++ main.py
@@ -131,3 +131,20 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # for i in text:
 
+# Step 19
+
+# The code to execute at each iteration — placed after the : — constitutes the body of the loop. This code must be indented. In Python, it is recommended to use 4 spaces per indentation level. This indented level is a code block.
+
+    # for i in text:
+    #     <code>
+
+# Give your for loop a body by adding a call to print(i). Remember to indent the loop body.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in text:
+    print(i)
+    
+    
