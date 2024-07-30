b71dd6d469c7fdcce1a47499c3d77a51101e3278
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:35:10 +0200

    feat: Instead of assigning alphabet[new_index] to encrypted_text, assign the current value of encrypted_text plus alphabet[new_index] to this variable.

--- main.py
+++ main.py
@@ -259,4 +259,15 @@ for char in text.lower():
     encrypted_text = alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 35
+
+# Instead of assigning alphabet[new_index] to encrypted_text, assign the current value of encrypted_text plus alphabet[new_index] to this variable.
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text = encrypted_text + alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+
+
 
