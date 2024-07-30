147e33ecb7579c6963076f59ee4c67ec38f6288b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:33:26 +0200

    feat: create a new_char variable at the end of your loop body. Set its value to alphabet[new_index].

--- main.py
+++ main.py
@@ -201,4 +201,31 @@ for char in text.lower():
     print(char, index)
     new_index = index + shift
 
+# Step 30
+
+# Now you need to create a new_char variable at the end of your loop body. Set its value to alphabet[new_index].
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print(new_char)
+
+# Clean Output
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
+
 
