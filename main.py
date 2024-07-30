682ad84493b4d63291084b423da7210280e1c96b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:23:37 +0200

    feat: Declare a variable named shifted and assign it the alphabet letter at index plus shift

--- main.py
+++ main.py
@@ -100,3 +100,17 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 index = alphabet.find(text[0].lower())
 print(index)
+
+# Step 16
+
+# As you can see from the output, "h" is at index 7 in the alphabet string. Now you need to find the letter at index 7 plus the value of shift. For that, you can use the addition operator, +, in the same way you would use it for a mathematical addition.
+# Declare a variable named shifted and assign it the alphabet letter at index plus shift.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index + shift]
+print(shifted)
+
