ae359703710b6027afe87bcfabf1571f9c1d90e0
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:42:03 +0200

    feat: If you wish to incorporate additional characters into the alphabet string, such as digits or special characters,  it's necessary to manually modify the right operand of the modulo operation. Replace 26 with len(alphabet) to avoid this issue.

--- main.py
+++ main.py
@@ -392,5 +392,44 @@ for char in text.lower():
         encrypted_text += alphabet[new_index]
         print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 43
+
+# If you wish to incorporate additional characters into the alphabet string, 
+# such as digits or special characters, 
+# you'll find it's necessary to manually modify the right operand of the modulo operation.
+
+# Replace 26 with len(alphabet) to avoid this issue.
+ 
+text = 'Hello Zaira'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+        print('char:', char, 'encrypted text:', encrypted_text)
+
+# Std Outuput
+
+text = 'Hello Zaira'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+
+print('plain text:', text)
+print('encrypted text:', encrypted_text)
 
 
