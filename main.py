3584d60cfb2356b1ef85ceb5eecb968ec0933d71
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:34:42 +0200

    feat: replace new_char with encrypted_text. Also, modify the print() call to reflect this change.

--- main.py
+++ main.py
@@ -244,4 +244,19 @@ for char in text.lower():
     new_char = alphabet[new_index]
     print('char:', char, 'new char:', new_char)
 
+# Step 34
+
+# Now, replace new_char with encrypted_text. Also, modify the print() call to reflect this change.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    index = alphabet.find(char)    
+    new_index = index + shift
+    encrypted_text = alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+
 
