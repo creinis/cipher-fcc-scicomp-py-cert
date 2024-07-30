6297360c387e65ca90de68616fb322b4fd570a91
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:55:27 +0200

    feat: f-strings enable you to interpolate values in your strings. Modify the first print() call to use an f-string.

--- main.py
+++ main.py
@@ -740,3 +740,21 @@ print('Key: '+ custom_key)
 decryption = decrypt(encryption, custom_key)
 print('Decrypted text: ' + decryption)
 
+# Step 87
+
+# In Python, there's a way to easily format strings. 
+# f-strings enable you to interpolate values in your strings.
+
+# Interpolation means writing placeholders that will be replaced by the specified values when the program runs. 
+# For example, you can get the same result of 
+# 'Encrypted text: ' + text with f'Encrypted text: {text}'. 
+# You need to put an f before the quotes to create the f-string and write the variables or expressions you want to interpolate between curly braces.
+
+# Modify the first print() call to use an f-string.
+
+print(f'Encrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'Decrypted text: {decryption}')
+
+
