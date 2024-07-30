270cbec9263d2b692bda4341c05c0ba161a3a558
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:18:18 +0200

    feat: modify your existing print() call to print the last character in your string

--- main.py
+++ main.py
@@ -18,3 +18,10 @@ print(text)
 # Now, instead of printing text, print just the character at index 6
 
 print(text[6])
+
+# Step 5
+# You can also access string characters starting from the end of the string. The last character has an index of -1, the second to last -2 and so on. 
+# Now modify your existing print() call to print the last character in your string.
+
+print(text[-1])
+
