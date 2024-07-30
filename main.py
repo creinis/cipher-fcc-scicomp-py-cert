cd8979af3e9ca04dbae82d2d2f27642bbc6ae9b1
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:40:43 +0200

    feat: # When the loop reaches the letter Z, the sum index + shift exceeds the length of the string alphabet. Modulo operator to return the remainder of the division between two numbers. Surround index + shift with parentheses, and modulo the expression with 26, which is the alphabet length.

--- main.py
+++ main.py
@@ -370,5 +370,27 @@ for char in text.lower():
         encrypted_text += alphabet[new_index]
         print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 42
+
+# When the loop reaches the letter Z, the sum index + shift exceeds the length of the string alphabet.
+# In this case, the modulo operator, %, can be used to return the remainder of the division between two numbers. 
+# For example: 5 % 2 is equal to 1, because 5 divided by 2 has a quotient of 2 and a remainder of 1.
+
+# Surround index + shift with parentheses, and modulo the expression with 26, which is the alphabet length.
+
+text = 'Hello Zaira'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift)% 26
+        encrypted_text += alphabet[new_index]
+        print('char:', char, 'encrypted text:', encrypted_text)
+
 
 
