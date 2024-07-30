ee1523e4a11d34f421e618868f76255132bb255a
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:36:01 +0200

    feat: print the result of comparing char with an empty space

--- main.py
+++ main.py
@@ -269,5 +269,35 @@ for char in text.lower():
     encrypted_text = encrypted_text + alphabet[new_index]
     print('char:', char, 'encrypted text:', encrypted_text)
 
+# Step 37
+
+# Comparison operators allow you to compare two objects based on their values. 
+# You can use a comparison operator by placing it between the objects you want to compare. 
+# They return a Boolean value — namely True or False — depending on the truthfulness of the expression.
+
+    # Python has the following comparison operators:
+    # Operator 	Meaning
+    # == 	Equal
+    # != 	Not equal
+    # > 	Greater than
+    # < 	Less than
+    # ≥ 	Greater than or equal to
+    # ≤ 	Less than or equal to
+
+# At the top of your loop, print the result of comparing char with an empty space. 
+# Use the equality operator == for that.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    print(char == ' ')
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+
 
 
