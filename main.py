2ce1a984a62452dc4e22580a4de8dc7fe7525c25
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:20:39 +0200

    feat: cipher logic: Remove both calls to print() and declare another variable called alphabet. Assign the string abcdefghijklmnopqrstuvwxyz to this variable.

--- main.py
+++ main.py
@@ -49,3 +49,17 @@ shift = 3
 print(shift)
 print (type(shift))
 
+# Step 11
+
+# Key aspects of variable naming in Python are:
+
+#    Some words are reserved keywords (e.g. for, while, True). They have a special meaning in Python, so you cannot use them for variable names.
+#    Variable names cannot start with a number, and they can only contain alpha-numeric characters or underscores.
+#    Variable names are case sensitive, i.e. my_var is different to my_Var and MY_VAR.
+#    Finally, it is a common convention to write variable names using snake_case, where each space is replaced by an underscore character and the words are written in lowercase letters.
+
+# Remove both calls to print() and declare another variable called alphabet. Assign the string abcdefghijklmnopqrstuvwxyz to this variable.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
