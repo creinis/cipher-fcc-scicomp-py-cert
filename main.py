915aff7af4c215d3f5dae6129196a719881e147b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:44:44 +0200

    feat: This algorithm is referred to as the Vigenère cipher, where the offset for each letter is determined by another text, called the key.
    Ttransform Caesar cipher into a Vigenère cipher by removing the two function calls.

--- main.py
+++ main.py
@@ -498,4 +498,33 @@ def caesar(message, offset):
 
 caesar(text, shift)
 
+# Step 52 - Vigenère Cipher
+
+# Currently, every single letter is always encrypted with the same letter, depending on the specified offset. 
+# What if the offset were different for each letter? That would be much more difficult to decrypt. 
+# This algorithm is referred to as the Vigenère cipher, where the offset for each letter is determined by another text, called the key.
+
+# Start transforming your Caesar cipher into a Vigenère cipher by removing the two function calls.
+
+text = 'Hello Zaira'
+custom_key = 'python'
+
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:
+            key_char = key[key_index % len(key)]
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
 
