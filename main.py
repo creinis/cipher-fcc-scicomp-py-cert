b5016b04e17dbea87e77ff7852d6e92869e304e7
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 12:51:06 +0200

    feat: function can be used both to encrypt and decrypt a message now. Clean up code with better variable names. Change each occurrence of encrypted_text into final_message

--- main.py
+++ main.py
@@ -642,4 +642,33 @@ print(encryption)
 decryption = vigenere(encryption, custom_key, -1)
 print(decryption)
 
+# Step 70
+
+# Now, your function can be used both to encrypt and decrypt a message. 
+# Clean up your code with better variable names.
+
+# Change each occurrence of encrypted_text into final_message.
+
+def vigenere(message, key, direction):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            final_message += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
 
